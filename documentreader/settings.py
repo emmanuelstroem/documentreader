9d5287ee788347291236b2b0986f314e6819d31b
Emmanuel <emmanuel.stroem@gmail.com>
2020-01-27 01:53:55 +0800

    Add root url path and add environment variables to setting.py

--- documentreader/settings.py
+++ documentreader/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qd208a-8$9qklo71vw(ue32xwl(g4kqng!8=j@onb!-czb(ehs'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'qd208a-8$9qklo71vw(ue32xwl(g4kqng!8=j@onb!-czb(ehs')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
