07463d5167b1dc6ca6d47b0cd147e82812460417
Emmanuel <emmanuel.stroem@gmail.com>
2020-01-27 13:54:16 +0800

    Add Dockerfile and prometheus library for Django

--- documentreader/settings.py
+++ documentreader/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reader',
+    'django_prometheus',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'documentreader.urls'
